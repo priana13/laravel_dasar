e2024d7a32ab70f2cefb5cda3e7be0d7d81721b2
priana <herian.sap@gmail.com>
2021-04-15 20:49:09 +0700

    Cara redirect

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,7 @@ class HomeController extends Controller
     public function index()
     {
 
-        return view('home');
+        return redirect()->to('form');
     }
 
     public function store(Request $request)
