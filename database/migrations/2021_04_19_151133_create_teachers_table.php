d8746c960b8f532d5560c7dcbdda8de9ce02a401
priana13 <herian.sap@gmail.com>
2021-04-19 22:28:14 +0700

    Migration Lara 5> cukup .. create_articles_table

---
+++ database/migrations/2021_04_19_151133_create_teachers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name',50);
+            $table->string('nip',10);
+            $table->string('gelar',10);
+            $table->enum('gender',['L','P']);
+            $table->string('email')->nullable()->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teachers');
+    }
+}
