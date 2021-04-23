18abaaab27908292baf2e3b4e686d001046c5f24
priana <herian.sap@gmail.com>
2021-04-23 20:48:28 +0700

    FindOrFind untuk arahkan ke 404 jika data kosong

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -83,7 +83,7 @@ class PostController extends Controller
      */
     public function edit($id)
     {
-        $post = Post::find($id);
+        $post = Post::findOrFail($id);
 
         // Cara Update pertama
         // $post->update([
