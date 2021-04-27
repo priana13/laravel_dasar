89a2696b5c7d28cbde52f0d907e1cc3b37bd5acd
priana13 <herian.sap@gmail.com>
2021-04-28 03:09:56 +0700

    mnggunakan Faker Genaretor pada Seed (cli db:seed)

--- database/seeds/PostTableSeeder.php
+++ database/seeds/PostTableSeeder.php
@@ -2,9 +2,18 @@
 
 use Illuminate\Database\Seeder;
 use App\Post;
+use Faker\Generator as Faker;
 
 class PostTableSeeder extends Seeder
 {
+    protected $post;
+    protected $faker;
+
+    public function __construct(Post $post, Faker $faker)
+    {
+        $this->post = $post;
+        $this->faker = $faker;
+    }
     /**
      * Run the database seeds.
      *
@@ -12,11 +21,14 @@ class PostTableSeeder extends Seeder
      */
     public function run()
     {
-        Post::create([
-            'title' => 'Title 1',
-            'content' => 'Konten 1',
-            'slug'=>uniqid(),
-            'is_published' => 0
-        ]);
+        foreach (range(1,100) as $x) {
+       
+            Post::create([
+                'title' => $this->faker->sentence(5),
+                'content' => $this->faker->sentence(100),
+                'slug'=>uniqid(),
+                'is_published' => 0
+            ]);
+         }
     }
 }
